f9fc58e55366f2233bdd6d47b868405e71bb2e04
egoing <egoing@gmail.com>
2018-06-14 00:43:55 +0900

    4.1.MySQL로 Read 기능 구현 1

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
+var mysql = require('mysql');
+var db = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'111111',
+  database:'opentutorials'
+});
+db.connect();
+
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -12,7 +21,7 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        fs.readdir('./data', function(error, filelist){
+        /* fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
           var list = template.list(filelist);
@@ -22,6 +31,11 @@ var app = http.createServer(function(request,response){
           );
           response.writeHead(200);
           response.end(html);
+        }); */
+        db.query(`SELECT * FROM topic`, function(error,topics){
+          console.log(topics);
+          response.writeHead(200);
+          response.end('Success');
         });
       } else {
         fs.readdir('./data', function(error, filelist){
