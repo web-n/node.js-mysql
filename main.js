14095005bd8e664fe9bb0cebdfcdd48d8b2cf5cf
egoing <egoing@gmail.com>
2018-06-15 00:23:35 +0900

    10.MySQL JOIN - 글 생성 구현

--- main.js
+++ main.js
@@ -65,24 +65,29 @@ var app = http.createServer(function(request,response){
       }
     } else if(pathname === '/create'){
       db.query(`SELECT * FROM topic`, function(error,topics){
-        var title = 'Create';
-        var list = template.list(topics);
-        var html = template.HTML(title, list,
-          `
-          <form action="/create_process" method="post">
-            <p><input type="text" name="title" placeholder="title"></p>
-            <p>
-              <textarea name="description" placeholder="description"></textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
-          `,
-          `<a href="/create">create</a>`
-        );
-        response.writeHead(200);
-        response.end(html);
+        db.query('SELECT * FROM author', function(error2, authors){
+          var title = 'Create';
+          var list = template.list(topics);
+          var html = template.HTML(title, list,
+            `
+            <form action="/create_process" method="post">
+              <p><input type="text" name="title" placeholder="title"></p>
+              <p>
+                <textarea name="description" placeholder="description"></textarea>
+              </p>
+              <p>
+                ${template.authorSelect(authors)}
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+            `,
+            `<a href="/create">create</a>`
+          );
+          response.writeHead(200);
+          response.end(html);
+        });
       });
     } else if(pathname === '/create_process'){
       var body = '';
@@ -94,7 +99,7 @@ var app = http.createServer(function(request,response){
           db.query(`
             INSERT INTO topic (title, description, created, author_id) 
               VALUES(?, ?, NOW(), ?)`,
-            [post.title, post.description, 1], 
+            [post.title, post.description, post.author], 
             function(error, result){
               if(error){
                 throw error;
