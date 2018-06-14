ac537f223d11824804817d1bcfe710679efcdbb2
egoing <egoing@gmail.com>
2018-06-14 18:12:48 +0900

    9.MySQL JOIN - 글 상세보기 구현

--- main.js
+++ main.js
@@ -37,15 +37,20 @@ var app = http.createServer(function(request,response){
          if(error){
            throw error;
          }
-         db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2, topic){
+         db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`,[queryData.id], function(error2, topic){
            if(error2){
              throw error2;
            }
+           console.log(topic);
           var title = topic[0].title;
           var description = topic[0].description;
           var list = template.list(topics);
           var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
+            `
+            <h2>${title}</h2>
+            ${description}
+            <p>by ${topic[0].name}</p>
+            `,
             ` <a href="/create">create</a>
                 <a href="/update?id=${queryData.id}">update</a>
                 <form action="delete_process" method="post">
