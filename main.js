c06d0589b5b63a6f9715f8e06ff0013cfab9c7e8
egoing <egoing@gmail.com>
2018-06-14 16:32:05 +0900

    8.MySQL로 글삭제 기능 구현

--- main.js
+++ main.js
@@ -147,12 +147,13 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var id = post.id;
-          var filteredId = path.parse(id).base;
-          fs.unlink(`data/${filteredId}`, function(error){
+          db.query('DELETE FROM topic WHERE id = ?', [post.id], function(error, result){
+            if(error){
+              throw error;
+            }
             response.writeHead(302, {Location: `/`});
             response.end();
-          })
+          });
       });
     } else {
       response.writeHead(404);
