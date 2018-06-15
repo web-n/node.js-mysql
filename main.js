e37c7004c25a0c75328e799fc770d24d62dff537
egoing <egoing@gmail.com>
2018-06-15 14:09:13 +0900

    13.정리정돈-db접속

--- main.js
+++ main.js
@@ -1,19 +1,8 @@
 var http = require('http');
-var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
-var mysql = require('mysql');
-var db = mysql.createConnection({
-  host:'localhost',
-  user:'root',
-  password:'111111',
-  database:'opentutorials'
-});
-db.connect();
-
+var db = require('./lib/db');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
