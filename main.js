0d75ae93acb7aef2566c458776caba98e77a066f
egoing <egoing@gmail.com>
2018-06-16 02:04:30 +0900

    16.저자 목록 보기 기능 구현

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 var topic = require('./lib/topic');
+var author = require('./lib/author');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -22,6 +23,8 @@ var app = http.createServer(function(request,response){
       topic.update_process(request, response);
     } else if(pathname === '/delete_process'){
       topic.delete_process(request, response);
+    } else if(pathname === '/author'){
+      author.home(request, response);
     } else {
       response.writeHead(404);
       response.end('Not found');
