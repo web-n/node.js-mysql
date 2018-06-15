bf51c023b7a4cbc1cf0471951d656f67533b41b0
egoing <egoing@gmail.com>
2018-06-16 00:22:54 +0900

    Revert "14.3.정리정돈-topic 3"
    This reverts commit 4e6b400ca4d9a80b0a7654b0ef5d257bb6b153e6.

--- main.js
+++ main.js
@@ -20,7 +20,40 @@ var app = http.createServer(function(request,response){
     } else if(pathname === '/create_process'){
       topic.create_process(request, response);
     } else if(pathname === '/update'){
-      topic.update(request, response);
+      db.query('SELECT * FROM topic', function(error, topics){
+        if(error){
+          throw error;
+        }
+        db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id], function(error2, topic){
+          if(error2){
+            throw error2;
+          }
+          db.query('SELECT * FROM author', function(error2, authors){
+            var list = template.list(topics);
+            var html = template.HTML(topic[0].title, list,
+              `
+              <form action="/update_process" method="post">
+                <input type="hidden" name="id" value="${topic[0].id}">
+                <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
+                <p>
+                  <textarea name="description" placeholder="description">${topic[0].description}</textarea>
+                </p>
+                <p>
+                  ${template.authorSelect(authors, topic[0].author_id)}
+                </p>
+                <p>
+                  <input type="submit">
+                </p>
+              </form>
+              `,
+              `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
+            );
+            response.writeHead(200);
+            response.end(html);
+          });
+          
+        });
+      });
     } else if(pathname === '/update_process'){
       var body = '';
       request.on('data', function(data){
