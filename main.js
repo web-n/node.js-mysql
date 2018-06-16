36492de8c66d017a96fd872bebdc18701cc9ab37
egoing <egoing@gmail.com>
2018-06-16 12:53:55 +0900

    19.저자 삭제 기능 구현

--- main.js
+++ main.js
@@ -31,6 +31,8 @@ var app = http.createServer(function(request,response){
       author.update(request, response);
     } else if(pathname === '/author/update_process'){
       author.update_process(request, response);
+    } else if(pathname === '/author/delete_process'){
+      author.delete_process(request, response);
     } else {
       response.writeHead(404);
       response.end('Not found');
